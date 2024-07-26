82cbf312467f0e14d69d72e6421b0fc5822da00b
yk <federico@pietrolucci.net>
2024-07-26 21:26:19 +0200

    fix: move the setup code to new().

--- lua/cmp_org_roam/init.lua
+++ lua/cmp_org_roam/init.lua
@@ -2,10 +2,23 @@
 
 local source = {}
 
-local registered = false
-
 source.new = function()
-  return setmetatable({}, { __index = source })
+  local instance = setmetatable({}, { __index = source })
+  local has_cmp, _ = pcall(require, 'cmp')
+  if not has_cmp then
+    return
+  end
+
+  local has_org_roam, org_roam = pcall(require, 'org-roam')
+  if not has_org_roam then
+    return
+  end
+
+  instance.org_roam_db = org_roam.database:path()
+  if vim.fn.filereadable(instance.org_roam_db) == 0 then
+    return
+  end
+  return instance
 end
 
 source.get_keyword_pattern = function()
@@ -14,6 +27,7 @@ end
 
 function source:complete(request, callback)
   -- Ingest the org-roam database.
+  -- FIXIT: sync with db updates (e.g. new nodes).
   local db = vim.fn.json_decode(vim.fn.readfile(self.org_roam_db))
   local input =
     string.sub(request.context.cursor_before_line, request.offset - 1)
@@ -55,26 +69,6 @@ function source:complete(request, callback)
   }
 end
 
-source.setup = function()
-  if registered then
-    return
-  end
-  registered = true
-
-  local has_cmp, _ = pcall(require, 'cmp')
-  if not has_cmp then
-    return
-  end
-
-  local has_org_roam, org_roam = pcall(require, 'org-roam')
-  if not has_org_roam then
-    return
-  end
-
-  source.org_roam_db = org_roam.database:path()
-  if vim.fn.filereadable(source.org_roam_db) == 0 then
-    return
-  end
-end
+source.setup = function() end
 
 return source
