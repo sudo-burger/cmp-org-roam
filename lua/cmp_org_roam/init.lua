57df1212f0a466fafb1b8a6cdc2e9e0d7fad5d50
yk <federico@pietrolucci.net>
2024-08-02 01:47:28 +0200

    fix: cleanups/performance.

--- lua/cmp_org_roam/init.lua
+++ lua/cmp_org_roam/init.lua
@@ -23,45 +23,52 @@ end
 
 function source:complete(request, callback)
   require('org-roam').database:files({ force = false }):next(function(files)
+    local items = {}
     local input =
       string.sub(request.context.cursor_before_line, request.offset - 1)
-    local items = {}
+    local line = request.context.cursor.row - 1
+    local start_char = request.context.cursor.col - 1 - #input
+    local end_char = request.context.cursor.col - 1
     for _, file in pairs(files.files) do
       local node_id = file:get_property 'id'
 
       -- Skip files that have no node_id or that are not valid org files.
-      if node_id and file.parser._valid ~= false then
-        -- In theory a node could have no title. Use the node's ID as fallback.
-        local node_title = file:get_title() or node_id
-        local title_and_aliases = { node_title }
+      if not node_id or file.parser._valid == false then
+        goto continue
+      end
+      -- In theory a node could have no title. Use the node's ID as fallback.
+      local node_title = file:get_title() or node_id
+      local title_and_aliases = { node_title }
 
-        -- Aliases are stored as space-separated strings.
-        -- Split here.
-        local node_aliases = file:get_property 'roam_aliases' or ''
-        for str in string.gmatch(node_aliases, '([^%s]+)') do
+      -- Aliases are stored as space-separated, double-quoted strings.
+      -- Split here.
+      local node_aliases = file:get_property 'roam_aliases'
+      if node_aliases then
+        for str in string.gmatch(node_aliases, '"(.-)"') do
           table.insert(title_and_aliases, str)
         end
+      end
 
-        for _, node_name in ipairs(title_and_aliases) do
-          table.insert(items, {
-            filterText = node_name,
-            label = node_name,
-            textEdit = {
-              newText = '[[id:' .. node_id .. '][' .. node_name .. ']]',
-              range = {
-                start = {
-                  line = request.context.cursor.row - 1,
-                  character = request.context.cursor.col - 1 - #input,
-                },
-                ['end'] = {
-                  line = request.context.cursor.row - 1,
-                  character = request.context.cursor.col - 1,
-                },
+      for _, node_name in ipairs(title_and_aliases) do
+        table.insert(items, {
+          filterText = node_name,
+          label = node_name,
+          textEdit = {
+            newText = '[[id:' .. node_id .. '][' .. node_name .. ']]',
+            range = {
+              start = {
+                line = line,
+                character = start_char,
+              },
+              ['end'] = {
+                line = line,
+                character = end_char,
               },
             },
-          })
-        end
+          },
+        })
       end
+      ::continue::
     end
     callback {
       items = items,
