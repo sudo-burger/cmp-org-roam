63dce8f718993082dfd14d53c3921febf68afead
yk <federico@pietrolucci.net>
2024-08-01 18:00:04 +0200

    fix: use org-roam's async API.

--- lua/cmp_org_roam/init.lua
+++ lua/cmp_org_roam/init.lua
@@ -34,41 +34,45 @@ function source:complete(request, callback)
   -- In jq, the query would be '.nodes[]|.aliases[],.title|select(. != [])
   local items = {}
   local roam = require 'org-roam'
-  local files = roam.database:files_sync()
-  for _, file in pairs(files.files) do
-    local node_id = file:get_property 'id'
-    if node_id and file.parser._valid ~= false then
-      local node_title = file:get_title() or node_id
-      local title_and_aliases = {}
-      table.insert(title_and_aliases, node_title)
-      local node_aliases = file:get_property 'roam_aliases' or ''
-      for str in string.gmatch(node_aliases, '([^%s]+)') do
-        table.insert(title_and_aliases, str)
-      end
+  local working = true
+  roam.database:files():next(function(files)
+    for _, file in pairs(files.files) do
+      local node_id = file:get_property 'id'
+      if node_id and file.parser._valid ~= false then
+        local node_title = file:get_title() or node_id
+        local title_and_aliases = {}
+        table.insert(title_and_aliases, node_title)
+        local node_aliases = file:get_property 'roam_aliases' or ''
+        for str in string.gmatch(node_aliases, '([^%s]+)') do
+          table.insert(title_and_aliases, str)
+        end
 
-      for _, v in ipairs(title_and_aliases) do
-        table.insert(items, {
-          filterText = v,
-          label = v,
-          textEdit = {
-            newText = '[[id:' .. node_id .. '][' .. v .. ']]',
-            range = {
-              start = {
-                line = request.context.cursor.row - 1,
-                character = request.context.cursor.col - 1 - #input,
-              },
-              ['end'] = {
-                line = request.context.cursor.row - 1,
-                character = request.context.cursor.col - 1,
+        for _, v in ipairs(title_and_aliases) do
+          table.insert(items, {
+            filterText = v,
+            label = v,
+            textEdit = {
+              newText = '[[id:' .. node_id .. '][' .. v .. ']]',
+              range = {
+                start = {
+                  line = request.context.cursor.row - 1,
+                  character = request.context.cursor.col - 1 - #input,
+                },
+                ['end'] = {
+                  line = request.context.cursor.row - 1,
+                  character = request.context.cursor.col - 1,
+                },
               },
             },
-          },
-        })
+          })
+        end
       end
     end
-  end
+    working = false
+  end)
   callback {
     items = items,
+    isIncomplete = working,
   }
 end
 
